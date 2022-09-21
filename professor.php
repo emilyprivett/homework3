7cbf74ecdd133048b4be5fe0f3aad8f08f10bdde
Emily Privett <eaprivett@ou.edu>
2022-09-21 14:22:53 -0500

    try 353535134

--- professor.php
+++ professor.php
@@ -47,6 +47,12 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+$sql = "SELECT * from Professor";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
 ?>
 
     <table class="table table-info">
