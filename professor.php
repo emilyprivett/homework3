bd90669e6c02ebfdd57ab7997f9bd41c5afd070d
Emily Privett <eaprivett@ou.edu>
2022-09-21 14:06:12 -0500

    added pages

--- professor.php
+++ professor.php
@@ -46,7 +46,7 @@ if ($conn->connect_error) {
     </ul>
   </div>
 </nav>
-    <h1 style="align-content:center;">Homework 3: Databases!!</h1>
+    <h1 style="align-content:center;">Professors</h1>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
