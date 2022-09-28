a05c932baa17bc314de735b3bca74decd8d0d2ff
Emily Privett <eaprivett@ou.edu>
2022-09-28 14:39:50 -0500

    attempt

--- studentcourse.php
+++ studentcourse.php
@@ -52,8 +52,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT StudentFirstName, StudentLastName, CourseName FROM Student s JOIN Course c ON st.SchoolID=sc.SchoolID";
-$result = $conn->query($sql);
+$sid = $_POST['id'];
+$sql = ""SELECT StudentFirstName, StudentLastName, CourseName FROM Student s JOIN Course c ON st.SchoolID=sc.SchoolID" . $sid;
+//echo $sql;
+    $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
