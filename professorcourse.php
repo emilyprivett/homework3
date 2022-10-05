8493856d0ebdd3f467432ea296fe901536728a92
Emily Privett <eaprivett@ou.edu>
2022-10-04 21:26:33 -0500

    last try

--- professorcourse.php
+++ professorcourse.php
@@ -62,7 +62,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 $pid = $_POST['id'];
-$sql = "SELECT CourseName, ProfessorFirstName, ProfessorLastName, FROM Course c JOIN Professor p ON c.ProfessorID=p.ProfessorID WHERE p.ProfessorID=" . $pid;
+$sql = "SELECT CourseName, ProfessorFirstName, ProfessorLastName, FROM Course c JOIN Professor p ON c.ProfessorID=p.ProfessorID WHERE p.ProfessorID=" .$pid;
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
