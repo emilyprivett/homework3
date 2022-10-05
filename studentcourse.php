88a2763f915062334d78662d57dd11798500ff70
Emily Privett <eaprivett@ou.edu>
2022-10-04 20:00:42 -0500

    again

--- studentcourse.php
+++ studentcourse.php
@@ -62,7 +62,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 $sid = $_POST['id'];
-$sql = "SELECT StudentFirstName, StudentLastName, CourseName FROM Course c JOIN Student s ON c.CourseID=s.CourseID WHERE c.CourseID=". $sid;
+$sql = "SELECT CourseName, StudentFirstName, StudentLastName FROM Course c JOIN Student s ON c.CourseID=s.CourseID WHERE c.CourseID=" . $sid;
     $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
